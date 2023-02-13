75ce811e09ed5d596348a9abd453dc26157c2c72
Nick Ririnui <nik-r07@live.com>
2023-02-13 17:25:53 +1300

    update css

--- index.js
+++ index.js
@@ -35,19 +35,15 @@ let index = 0 // This will track our current image
 //Array to store imageNames
 let imageNameArray = ["Sacai Waffle (Black/White)", "Nike Dunk SB (Safari)", "Nike Air Max 98 (Mbappe)", "NB x Aime Leon Dore 550 (Gray)", "NB 2002r (Warm Alpacca)", "NB 990v5 (Gray)", "Jordan 1 (Mocha)", "Dr Marten 1461", "Yeezy Slide (Earth Brown)"]
 
-
-//for each method calls a function for each element in an array. The forEach() method is not executed for empty elements.
-
+//Function to update the POPUP Image & Name
 const updateImage = (i) => {
     let path = `Photos/${i+1}.webp`;
-    largeImage.src = path;
+    largeImage.src = path; // changes to what image I click on
     imageName.innerHTML = imageNameArray[`${i}`]; 
-    // imageIndex.innerHTML = `0${i+1}`
-    index = i;
-    
+    index = i
 }
 
-
+//for each method calls a function for each element in an array. .
 images.forEach((item, i) => {
     item.addEventListener('click', () => {
         updateImage(i)
@@ -56,12 +52,14 @@ images.forEach((item, i) => {
     })    
 })
 
-
+//Close POPUP
 closeBtn.addEventListener('click', () => {
     popup.classList.toggle('active');
 })
 
+//Both Arrow buttons call updateImage()
 
+//LEFT Arrow decreases index
 leftArrow.addEventListener('click', () => {
     if(index > -1) {
         updateImage(index - 1)
@@ -70,7 +68,7 @@ leftArrow.addEventListener('click', () => {
     }
     console.log(index);
 })
-
+//Right Arrow increases index 
 rightArrow.addEventListener('click', () => {
     if(index < images.length ) {
         updateImage(index + 1)
@@ -81,7 +79,24 @@ rightArrow.addEventListener('click', () => {
 
 
 
-// add a math operator
+// SCROLL Animation  2
+
+const boxes2 = document.querySelectorAll('.gallery')
+document.addEventListener('scroll', checkBoxes2)
+
+function checkBoxes2() {
+    // how much of the screen is shown before it triggers
+    const triggerBottom = window.innerHeight / 1 
+    boxes2.forEach(gallery => {
+        const boxTop = gallery.getBoundingClientRect().top
+        if(boxTop < triggerBottom) {
+            gallery.classList.add('show')
+        } else {
+            gallery.classList.remove('show')
+        }
+    })
+}
+ 
 
 
 
